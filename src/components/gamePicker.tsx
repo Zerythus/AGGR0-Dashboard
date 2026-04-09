8cd789fceb01776bba966c8a310ecbf55cf9e18b
Raizel Bacani <bacani.rai@gmail.com>
2026-04-08 17:27:17 -0700

    Removed flex wrap

--- src/components/gamePicker.tsx
+++ src/components/gamePicker.tsx
@@ -567,7 +567,7 @@ export default function GamePicker({ isSteamConnected, isEpicConnected }: GamePi
                                 }}
                             />
                         </div>
-                        <div className="flex justify-between items-center pr-5 pl-5 pb-5 gap-4 flex-wrap">              
+                        <div className="flex justify-between items-center pr-5 pl-5 pb-5 gap-4">              
                             <div className="flex items-center gap-3 ml-auto">
                                 <div className="flex items-center gap-1">
                                     <span className="text-(--secondary-text-color) text-sm">
