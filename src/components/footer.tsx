1521e6c9ed44ef34b4b912db6f26c9afc2554a40
Raizel Bacani <bacani.rai@gmail.com>
2026-04-21 20:19:04 -0700

    Added footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="flex items-center justify-center border-t border-white/10 bg-(--main-background-color) text-(--secondary-text-color) text-sm py-5">
+      © 2026 R.H.A.B. Studios. All rights reserved.
+    </footer>
+  );
+}
